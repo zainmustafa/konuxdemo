be17de2e5b83dea9e31bbb7a5f6dc8161719f2bb
zainmustafa <zmdhillo@gmail.com>
2018-08-27 21:37:30 +0500

    update epic.

--- app/scripts/epics/konux.js
+++ app/scripts/epics/konux.js
@@ -8,23 +8,15 @@ import { Observable } from "rxjs/Observable";
 import { ActionTypes } from "constants/index";
 import { apiHelper } from "../utils/konux";
 
-export function getApiData(action$, state) {
+export function getApiData(action$) {
     return action$.ofType(ActionTypes.ADD_TO_PLAYLIST_REQUEST).switchMap(() => {
         fromPromise(apiHelper())
-            .flatMap(data => {
-                const activeStep = state.getState().kit.activeStep + 1;
-
-                return Observable.of(
-                    {
-                        type: ActionTypes.CHOOSE_SPORT_SUCCESS,
-                        payload: data
-                    },
-                    {
-                        type: ActionTypes.STEPPER_COUNTER_UPDATE,
-                        payload: { activeStep }
-                    }
-                );
-            })
+            .flatMap(data =>
+                Observable.of({
+                    type: ActionTypes.CHOOSE_SPORT_SUCCESS,
+                    payload: data
+                })
+            )
             .catch(error =>
                 Observable.of(
                     {
