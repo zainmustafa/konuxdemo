6f6a5f00069ebe2adf5d064c2ce425072d9287d7
zainmustafa <zmdhillo@gmail.com>
2018-08-27 21:24:48 +0500

    add API helper.

---
+++ app/scripts/utils/konux.js
@@ -0,0 +1,15 @@
+// helper functions.
+
+export function apiHelper() {
+    return new Promise((resolve, reject) => {
+        fetch("http://konuxdata.getsandbox.com/data")
+            .then(response => response.json())
+            .then(function(data) {
+                console.log(data);
+                resolve(JSON.stringify(data));
+            })
+            .error(function(err){
+                reject(err);
+            })
+    });
+}
