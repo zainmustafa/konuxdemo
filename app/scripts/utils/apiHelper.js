72f47fd4235ce40644b95ad9b45a69dfca3868b8
zainmustafa <zmdhillo@gmail.com>
2018-08-29 00:19:52 +0500

    fix https error for cors.

--- app/scripts/utils/apiHelper.js
+++ app/scripts/utils/apiHelper.js
@@ -2,7 +2,7 @@
 
 export function apiHelper() {
     return new Promise((resolve, reject) => {
-        fetch("http://konuxdata.getsandbox.com/data")
+        fetch("https://konuxdata.getsandbox.com/data")
             .then(response => resolve(response.json()))
             .catch(err => reject(err))
     });
